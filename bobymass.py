850ee5d1dbd4bf59d8c04965e00ca0b0c1039fdb
caroltreacy <caroltreacy85@gmail.com>
2020-01-29 21:31:05 +0000

    added calculation BMI

---
+++ bobymass.py
@@ -0,0 +1,24 @@
+# calculate body mass in squared metres
+# weight 65
+# height 180
+# bmi 20.06
+
+
+weight = float(input("weight in kg"))
+height = float(input("height in cm"))
+
+heightsquared = height*height
+
+bmi = weight / heightsquared
+
+needed = bmi 
+
+
+print = ("your bmi is", needed , bmi: ,)
+
+
+
+
+
+
+
